0e3947b920c41a5661046c51d1d2623624f03d3b
Artem Bielykh <bielykh.artem@gmail.com>
2020-03-29 22:44:07 +0300

    Updated node env

--- config/config.js
+++ config/config.js
@@ -2,15 +2,15 @@ require('dotenv').config()
 
 const _ = require('lodash')
 
-const NODE_PORT = process.env.PORT || 4002
+const NODE_PORT = process.env.PORT || 4000
 
 const config = {
   development: {
     config_id: 'development',
     app_name: 'zoo-planeta-api',
     node_port: NODE_PORT,
-    db_url: 'mongodb://localhost:27017/admin',
-    // db_url: process.env.MONGODB_URI
+    // db_url: 'mongodb://localhost:27017/admin',
+    db_url: process.env.MONGODB_URI,
   },
   release: {
     config_id: 'release',
