689353dc642f4a81808a0d61d5283708e1c2cf4d
Artem Bielykh <bielykh.artem@gmail.com>
2020-04-16 00:09:52 +0300

    Added image uploader to AWS

--- controllers/uploader.js
+++ controllers/uploader.js
@@ -1,17 +1,12 @@
 require('dotenv').config()
-const cloudinary = require('cloudinary').v2
+
+const { uploadImage } = require('../utils/AWS')
 
 const uploadImageToCloudinary = async ctx => {
   let base64String = ctx.request.body.file
 
-  cloudinary.config({ 
-    cloud_name: process.env.CLOUD_NAME, 
-    api_key: process.env.CLOUD_API_KEY, 
-    api_secret: process.env.CLOUD_API_SECRET 
-  })
-
   try {
-    const uploadedImage = await cloudinary.uploader.upload(base64String)
+    const uploadedImage = await uploadImage(base64String)
     ctx.body = uploadedImage
 
   } catch (err) {
---
+++ utils/AWS/index.js
@@ -0,0 +1,87 @@
+const { S3 } = require("aws-sdk");
+const { getContentType } = require("content-type-to-ext");
+const uuid = require("uuid");
+
+const config = {
+  region: "us-east-2",
+  accessKeyId: '',
+  secretAccessKey: '',
+  Bucket: "zoo-planeta",
+  dirName: 'content/images'
+}
+
+const s3 = new S3({
+  region: config.region,
+  accessKeyId: config.accessKeyId,
+  secretAccessKey: config.secretAccessKey,
+});
+
+const parseImage = data => {
+  const reg = /^data:image\/([\w+]+);base64,([\s\S]+)/;
+  const match = data.match(reg);
+  const baseType = {
+    jpg: "jpg",
+    png: "png",
+    gif: "gif",
+    svg: "svg",
+    "svg+xml": "svg",
+  };
+  if (!match) {
+    throw new Error("image base64 data error");
+  }
+  const found = Object.entries(baseType).find(([key, value]) => key === match[1]);
+  const extName = found ? found[1] : match[1];
+  return {
+    mimeType: getContentType(extName),
+    buffer: match[2],
+    extName: `.${extName}`,
+  };
+};
+
+const uploadImage = async imageBuffer => {
+  try {
+    const fileName = uuid.v1();
+    const { mimeType, buffer, extName } = parseImage(imageBuffer);
+    return new Promise((resolve, reject) => {
+      s3.upload(
+        {
+          Key: `${fileName}${extName}`,
+          Body: Buffer.from(buffer, "base64"),
+          Bucket: config.Bucket,
+          ACL: "public-read",
+          ContentType: mimeType,
+        },
+        (err, data) => {
+          if (err) {
+            reject(err);
+          }
+          resolve(data.Location);
+        },
+      );
+    });
+  } catch (e) {
+    return Promise.resolve(imageBuffer);
+  }
+};
+
+const deleteImage = async fileName =>
+  new Promise((resolve, reject) => {
+    s3.deleteObject(
+      {
+        Bucket: config.Bucket,
+        Key: fileName,
+      },
+      (err, data) => {
+        if (err) {
+          reject(err);
+        }
+        resolve(data);
+      },
+    );
+  });
+
+module.exports = {
+  parseImage,
+  uploadImage,
+  deleteImage
+}
